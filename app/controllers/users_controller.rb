26d7893fec28ed705f924a588e23b6b464edce03
Gannon McGibbon <gannon.mcgibbon@gmail.com>
2021-04-21 17:08:11 -0400

    Use audio/x-wav format

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index
     blob = User.first.theme_song
 
-    response.headers["Content-Type"] = blob.content_type
+    response.headers["Content-Type"] = "audio/x-wav"
     response.headers["Content-Disposition"] = "inline"
 
     blob.download do |chunk|
